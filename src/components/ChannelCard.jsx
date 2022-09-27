168c16ad78df22c8c3e4bf3407402242ab1540d8
Roman Trynko <trynkoroman25@gmail.com>
2022-09-27 17:09:32 +0300

    fixed some issues

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -5,10 +5,7 @@ import { demoProfilePicture } from '../utils/constants';
 
 const ChannelCard = ({ channelDetail }) => {
   return (
-    <Box sx={{
-      boxShadow: 'none',
-      borderRadius: '20px',
-    }}>
+    <Box sx={{ boxShadow: 'none', borderRadius: '20px' }}>
       <Link to={`/channel/${channelDetail?.id?.channelId}`}>
         <CardContent sx={{
           display: 'flex',
@@ -18,7 +15,8 @@ const ChannelCard = ({ channelDetail }) => {
           color: '#fff'
         }}
         >
-          <CardMedia image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture} />
+          <CardMedia
+            image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture} alt={channelDetail?.snippet?.title} sx={{ borderRadius: '50%', height: '180px', width: '180px', mb: 2, border: '1px solid #e3e3e3' }} />
         </CardContent>
       </Link>
     </Box>
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import { Stack, Box } from '@mui/material';
-import { VideoCard, ChannelCard } from './';
+import React from "react";
+import { Stack, Box } from "@mui/material";
 
-const Videos = ({ videos }) => (
-  <Stack
-    direction={'row'}
-    flexWrap='wrap'
-    justifyContent='start'
-    gap={2}
-  >
-    {videos.map((item, index) => (
-      <Box key={index}>
-      {item.id.channelId && <ChannelCard channelDetail={item} />}
-      {item.id.videoId && <VideoCard video={item} />}
-      </Box>
-    ))}
-  </Stack>
-)
+import { ChannelCard, VideoCard } from "./";
 
-export default Videos
+const Videos = ({ videos, direction }) => {
+  return (
+    <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
+      {videos.map((item, idx) => {
+        return (
+          <Box key={idx}>
+            {item.id.videoId && <VideoCard video={item} />}
+            {item.id.channelId && <ChannelCard channelDetail={item} />}
+          </Box>
+        )
+      })}
+    </Stack>
+  );
+}
+
+export default Videos;
